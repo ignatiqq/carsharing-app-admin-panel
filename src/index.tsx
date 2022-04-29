08d9c36b5dab0fd803fd2d7cff639e7d9960f621
Ignat <zzzcap3@gmail.com>
2022-04-30 00:03:38 +0300

    gh-pages integration

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename='/carsharing-app-admin-panel'>
         <App />
       </Router>
     </Provider>
